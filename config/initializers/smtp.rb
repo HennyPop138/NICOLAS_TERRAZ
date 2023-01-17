a578171fe1fb8bb051e8acd020e177f0d44791f1
Anan Mignot <anan.wav@gmail.com>
2023-01-17 15:54:30 +0100

    set heroku environment variables

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -7,3 +7,14 @@ ActionMailer::Base.smtp_settings = {
   authentication: :login,
   enable_starttls_auto: true
 }
+
+
+# config.action_mailer.smtp_settings = {
+#   address: "smtp.gmail.com",
+#   port: 587,
+#   domain: "example.com",
+#   user_name: ENV["GMAIL_USERNAME"],
+#   password: ENV["GMAIL_APP_PASSWORD"],
+#   authentication: :plain,
+#   enable_starttls_auto: true
+# }
