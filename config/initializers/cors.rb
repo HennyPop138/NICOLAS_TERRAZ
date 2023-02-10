f7d25c55a13e64a95d56264232d41b9c9cc33f05
Anan Mignot <anan.wav@gmail.com>
2023-02-10 02:18:36 +0100

    changed initializers cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://www.terraz-plomberie.fr', 'https://www.terraz-plomberie.fr'
     resource '*', headers: :any, methods: [:get, :post, :options]
   end
 end
